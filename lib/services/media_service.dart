e45d469e120db64b30af98a54cbc795a48b014cd
D.Jayasimma <137266557+JAYASIMMA@users.noreply.github.com>
2025-12-29 05:39:57 +0530

    feat: Add media service for scanning local video and audio files with necessary Android permissions.

--- lib/services/media_service.dart
+++ lib/services/media_service.dart
@@ -13,6 +13,16 @@ class MediaService {
 
   Future<bool> requestPermissions() async {
     if (Platform.isAndroid) {
+      // For Android 11+ (API 30+)
+      if (await Permission.manageExternalStorage.status.isDenied) {
+        await Permission.manageExternalStorage.request();
+      }
+
+      if (await Permission.manageExternalStorage.status.isGranted) {
+        return true;
+      }
+
+      // For older Android versions
       Map<Permission, PermissionStatus> statuses = await [
         Permission.videos,
         Permission.audio,
@@ -40,6 +50,8 @@ class MediaService {
       '/storage/emulated/0/Download',
       '/storage/emulated/0/Music',
       '/storage/emulated/0/Podcasts',
+      '/storage/emulated/0/Telegram',
+      '/storage/emulated/0/Android/media/org.telegram.messenger/Telegram', // Newer Android versions
     ];
 
     for (final dirPath in directories) {
